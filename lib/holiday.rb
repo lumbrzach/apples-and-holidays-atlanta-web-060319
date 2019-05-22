d3be12a7ed2a2a498bc47ff6c2c8c2cf74c1b2e2
Zach Parker <parkerz410@gmail.com>
2019-05-22 13:50:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,13 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  
+  holiday_hash.each do | season, holiday|
+    puts "#{season.to_s.capitalize!}:"
+    holiday.each do |holiday, supplies|
+     puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{supplies.join(", ")}"
+     end
+ end
+end   
 
 end
 
