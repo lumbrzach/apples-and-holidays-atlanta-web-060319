9fd33d5b91903bb2cf47989ab5649930ac494edd
Zach Parker <parkerz410@gmail.com>
2019-05-22 12:58:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,7 +47,10 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   newarray = []
+
   holiday_hash[:winter].values << newarray
+  newarray.flatten = winter_supplies
+  winter_supplies
 end
 
 def all_supplies_in_holidays(holiday_hash)
